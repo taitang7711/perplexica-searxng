30f61752f8b927f8b0c29c1af14231f1eb3c7674
Tai Tang <taitang7711@gmail.com>
2026-02-04 18:20:48 +0700

    Initial commit - Perplexica with SearXNG configured

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import pkg from './package.json' with { type: 'json' };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        hostname: 's2.googleusercontent.com',
+      },
+    ],
+  },
+  serverExternalPackages: ['pdf-parse'],
+  outputFileTracingIncludes: {
+    '/api/**': [
+      './node_modules/@napi-rs/canvas/**',
+      './node_modules/@napi-rs/canvas-linux-x64-gnu/**',
+      './node_modules/@napi-rs/canvas-linux-x64-musl/**',
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_VERSION: pkg.version,
+  },
+};
+
+export default nextConfig;
